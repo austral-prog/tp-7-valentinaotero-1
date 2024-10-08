6673d83cc72faba2258447f57feeffef5bd34d9c
valentinaotero <162721713+valentinaotero@users.noreply.github.com>
2024-10-07 22:17:49 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,50 @@
+def index_of(word, list):
+    i = 0
+    while i < len(list):
+        if word == list[i]:
+            return i
+        i += 1
+    return -1
+
+
+
+
+
 def index_of_by_index(word, list, index):
+    i = 0
+    new_list = list[index:]
+    while i < len(new_list):
+        actual_word = new_list[i]
+        if actual_word == word:
+            return i + index
+        i += 1  
     return -1
 
 
 def index_of_empty(list):
-    return -1
-
-
-def index_of(word, list):
+    i = 0
+    while i < len(list):
+        actual_element = list[i]
+        if actual_element == "":
+            return i
+        i += 1
     return -1
 
 
 def put(word, list):
+    i = 0
+    for elemento in list:
+        if elemento == "":
+            list[i] = word
+            return i
+        i += 1
     return -1
 
 
 def remove(word, list):
-    return -1
+    elim = 0
+    for indice in range(len(list)):
+        if list[indice] == word:
+            list[indice] = ""
+            elim += 1
+    return elim
